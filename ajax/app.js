631365b898db312992f0bd2114d7131b31019cd0
deryacortuk <45028347+deryacortuk@users.noreply.github.com>
2019-08-14 00:10:48 +0300

    external api

---
+++ ajax/app.js
@@ -0,0 +1,21 @@
+document.getElementById("change").addEventListener("click", change);
+
+function change() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api.exchangeratesapi.io/latest");
+
+    xhr.onload = function() {
+        if (this.status) {
+            const response = JSON.parse(this.responseText);
+
+            const rate = response.rates.USD
+            const amount = Number(document.getElementById("amount").value);
+            document.getElementById("usd").value = amount * rate;
+        }
+    }
+
+    xhr.send();
+
+
+}
